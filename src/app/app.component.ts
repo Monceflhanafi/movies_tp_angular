c1a624814426d987e266690d4ba4f151b2f4ee0b
Monceflhanafi <polakomoncef@gmail.com>
2023-02-17 20:20:03 +0100

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'movies_tp_angular';
+  title = 'movies tp angular';
+  showImages = true;
+  ShowImage() {
+    this.showImages = !this.showImages;
+  }
+  movies = [
+    {
+      name: 'LE BLEU DU CAFTAN',
+      realisateur: 'Maryam Touzani',
+      pays: 'France, Maroc, Belgique, Danemark',
+      img: 'assets/images/Blue-Caftan.jpg',
+      starRating: 3,
+    },
+    {
+      name: 'ALMA VIVA',
+      realisateur: 'Cristèle Alves Meira',
+      pays: 'France, Portugal',
+      img: 'assets/images/Alma-Viva.jpeg',
+      starRating: 4,
+    },
+    {
+      name: 'RED SHOES',
+      realisateur: 'Carlos Kaiser Eichelmann',
+      pays: 'Mexique, Italie',
+      img: 'assets/images/Red-Shoes.jpg',
+      starRating: 5,
+    },
+    {
+      name: 'ASTRAKAN',
+      realisateur: 'David Depesseville',
+      pays: 'France',
+      img: 'assets/images/Astrakan.jpg',
+      starRating: 4,
+    },
+  ];
 }
